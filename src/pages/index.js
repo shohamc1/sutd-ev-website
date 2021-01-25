a30b7111cc03655f31bfce599e7742b15d9bd518
shohamc1 <shhmchk@gmail.com>
2021-01-26 00:42:31 +0800

    react helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 import * as React from "react";
 import NavBar from "../components/navbar";
+import { Helmet } from "react-helmet";
+import Favicon from "../assets/images/favicon.webp";
 
 const IndexPage = () => {
   return (
     <div class="flex justify-center">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>SUTD EV Prototype</title>
+        <link rel="shortcut icon" type="image/jpg" href={Favicon} />
+      </Helmet>
       <NavBar></NavBar>
     </div>
   );
