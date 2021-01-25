c8f63198e9d94deab17f5def12abbc4312a41aa2
shohamc1 <shhmchk@gmail.com>
2021-01-25 19:43:44 +0800

    remove google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,6 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-postcss",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-transformer-sharp",
